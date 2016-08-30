682212a810cd9d0aefa43c937bca7818c20c59dd
mephilip <me@philiparudy.com>
2016-08-30 14:17:28 -0400

    added some thangs

--- sites/all/themes/esdhomesite/templates/html.tpl.php
+++ sites/all/themes/esdhomesite/templates/html.tpl.php
@@ -12,10 +12,13 @@
 	<?php print $page_top; ?>
 	<?php print $page; ?>
 	<?php print $page_bottom; ?>
+	
+	
 	<div id="nygov-universal-footer" class="nygov-universal-container footer">
             <iframe id="nygov-universal-footer-frame" class="nygov-universal-container" width="100%" height="125px" src="//static-assets.ny.gov/load_global_footer/ajax?iframe=true" data-updated="2014-11-07 08:30" frameborder="0" style="border:none; overflow:hidden; width:100%; height:125px;" scrolling="no">
                 Your browser does not support iFrames
             </iframe>
     </div>
+    
 </body>
 </html>
