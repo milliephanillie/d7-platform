003b139fca3f06d18b3646e0f8220cb5b4f3e02a
mephilip <me@philiparudy.com>
2016-08-30 13:49:12 -0400

    added javascript tasks as as well as browserfy tasks

--- sites/all/themes/esdhomesite/gulpfile.js
+++ sites/all/themes/esdhomesite/gulpfile.js
@@ -1,55 +1,69 @@
 var gulp = require('gulp'),
-	minify = require('gulp-minify'),
+	browserSync  = require('browser-sync').create(),
+	watchify     = require('watchify'),
+	source       = require('vinyl-source-stream'),
+    browserify   = require('browserify'),
+    buffer       = require('vinyl-buffer'),
+    uglify       = require('gulp-uglify'),
 	sass = require('gulp-sass'),
 	autoprefixer = require('gulp-autoprefixer'),
-	cssmin = require('gulp-cssmin'),
 	rename = require('gulp-rename'),
-	autoguide = require('autoguide')
-	;
+	streamify    = require('gulp-streamify'),
+	cssmin = require('gulp-cssmin')
+	autoguide = require('./autoguide');
+	
+var onError = function(err) {
+    console.log(err.message);
+    this.emit('end');
+};
+	
+// bundling js with browserify and watchify
+var b = watchify(browserify(
+    [
+        'src/js/bundle/main'
+    ]
+));
+
+gulp.task('js', bundle);
+b.on('update', bundle);
+
+function bundle() {
+    return b.bundle()
+        .on('error', onError)
+        .pipe(source('bundle.js'))
+        .pipe(buffer())
+        .pipe(streamify(uglify()))
+        .pipe(gulp.dest('./assets/js'))
+        .pipe(browserSync.stream());
+}
 	
 gulp.task('sass', function () {
-	gulp.src('src/scss/*.scss')
-	 .pipe(sass().on('error', sass.logError))
-     .pipe(autoprefixer({
+	gulp.src('src/scss/**.scss')
+	.pipe(sass().on('error', sass.logError))
+	.pipe(autoprefixer({
         browsers: ['last 2 versions'],
         cascade: false
-     }))
-     .pipe(gulp.dest('css'))
-     .pipe(cssmin())
-     .pipe(rename({suffix: '.min'}))
-     .pipe(gulp.dest('css'))
-     ;
-});
-
-gulp.task('compress', function() {
-  gulp.src('src/js/*.js')
-    .pipe(minify({
-        ext:{
-            src:'.js',
-            min:'.min.js'
-        }
     }))
-    .pipe(gulp.dest('js'))
-    ;
+    .pipe(gulp.dest('assets/css'))
+    .pipe(cssmin())
+    .pipe(rename({suffix: '.min'}))
+    .pipe(gulp.dest('assets/css'));
 });
 
 gulp.task('watch', function () {
-	gulp.watch('src/scss/**/*.scss', ['sass']);
-	gulp.watch('src/js/**/*.js', ['compress']);
+	gulp.watch('src/scss/**/**/*.scss', ['sass']);
+	gulp.watch('src/js/bundle/*.js', ['js']);
 });
 
-gulp.task('autoguide', function() {
+gulp.task('autoguide', function (done) {
 	autoguide({
-	  src: [
-	    'scss'
-	  ],
-	  dest: 'public/styleguide',
-	  templateVars: {
-	    title: 'Super Awesome Styleguide'
-	  }
-	}, function (err, success) {
-	  if (!err)
-	    console.log ('Super Awesome Styleguide created!');
+		src: ['src/scss','src/js'],
+		vars: ['src/scss/utilities'],
+		styles: ['assets/css/main.css'],
+		dest: './'
+	}, function (err) {
+		done();
 	});
-});
+})
 
+	
